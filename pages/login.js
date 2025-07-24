9cda89c63c7ee81c3c90996c07b3cb23a8e3e821
jadjasbfhisadiojsaipd <161168891+jadjasbfhisadiojsaipd@users.noreply.github.com>
2025-07-24 19:41:38 +0200

    Add files via upload

---
+++ pages/login.js
@@ -0,0 +1,10 @@
+import '../public/style.css'
+
+export default function Login() {
+  return (
+    <div>
+      <h1>Login Page</h1>
+      <p>Login functionality coming soon...</p>
+    </div>
+  )
+}
