75ccfd5ffc8972145bedee36b910f986922d53e5
jadjasbfhisadiojsaipd <161168891+jadjasbfhisadiojsaipd@users.noreply.github.com>
2025-07-24 20:19:03 +0200

    Delete pages directory

--- pages/login.js
+++
@@ -1,8 +0,0 @@
-export default function Login() {
-  return (
-    <div>
-      <h1>Login Page</h1>
-      <p>Login functionality coming soon...</p>
-    </div>
-  )
-}
--- pages/prices.js
+++
@@ -1,8 +0,0 @@
-export default function Prices() {
-  return (
-    <div>
-      <h1>Pricing</h1>
-      <p>Basic - Free | Pro - $10/mo</p>
-    </div>
-  )
-}
